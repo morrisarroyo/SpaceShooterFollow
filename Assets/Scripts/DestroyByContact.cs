9711601f1f1a3fe8c7587f3c383215030c94ac11
Morris Arroyo <arroyomorris28@gmail.com>
2018-01-05 18:26:52 -0800

    Final working game

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,21 @@ public class DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	
+	GameController gameController;
+
+	void Start() {
+		GameObject gameControllerObject =  GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log("DestroyByContact - Cannot find 'GameController' script");
+		}
+	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary") 
@@ -13,7 +28,9 @@ public class DestroyByContact : MonoBehaviour
 		Instantiate(explosion, transform.position, transform.rotation);
 		if (other.tag == "Player") {
 			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
 		}
+		gameController.AddScore(scoreValue);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour 
 {
@@ -11,9 +12,41 @@ public class GameController : MonoBehaviour
 	public float startWait;
 	public float waveWait;
 
+	public GameObject scoreTextObject;
+	public GameObject restartTextObject;
+	public GameObject gameOverTextObject;
+	
+	int score;
+	bool gameOver;
+	bool restart;
+	
+	Text scoreText;
+    Text restartText;
+	Text gameOverText;
+
 	void Start()
 	{
+		scoreText = scoreTextObject.GetComponent<Text>();
+		restartText = restartTextObject.GetComponent<Text>();
+		gameOverText = gameOverTextObject.GetComponent<Text>();
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
 		StartCoroutine(SpawnWaves());
+	}
+
+	void Update ()
+	{
+		if (restart) 
+		{
+			if (Input.GetKeyDown (KeyCode.R)) 
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
 	}
 
 	IEnumerator SpawnWaves()
@@ -28,11 +61,29 @@ public class GameController : MonoBehaviour
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 		}
 	}
+    
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
 
-	// Update is called once per frame
-	void Update () {
-		
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text =  "Game Over!";
+		gameOver = true;
 	}
 }
